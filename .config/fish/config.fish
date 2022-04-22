7f1b4ba42637f422e7440193e1ad18216cf2c1e5
Alex Weininger <alex.weininger@live.com>
2022-04-21 23:46:36 -0700

    Edit fish colors

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -11,3 +11,12 @@ end
 set -x NVM_DIR ~/.nvm
 nvm use default --silent
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
+
+if test -d (brew --prefix)"/share/fish/completions"
+    set -gx fish_complete_path $fish_complete_path (brew --prefix)/share/fish/completions
+end
+
+if test -d (brew --prefix)"/share/fish/vendor_completions.d"
+    set -gx fish_complete_path $fish_complete_path (brew --prefix)/share/fish/vendor_completions.d
+end
+fish_add_path /usr/local/opt/libpq/bin
